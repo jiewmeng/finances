ab0bcb70d5085a71a1ab1558da5358044db6a39b
Jiew Meng <jiewmeng@gmail.com>
2019-04-07 21:56:55 +0800

    Remove config file

--- web/config/development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  GOOGLE_CLIENT_ID: '236282796861-e2doclesgv8tknbkh8un0ghaseortgpe.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: 'aALynZMtdGYFYL6OTgUreg7X',
-  GOOGLE_AUTH_REDIRECT_URL: 'http://localhost:8080',
-
-  FIREBASE_API_KEY: 'AIzaSyAL9RFWMnFN6R2LMEDO0lr0uMkoEKXd43Y',
-  FIREBASE_AUTH_DOMAIN: 'finances-jiewmeng.firebaseapp.com',
-  FIREBASE_PROJECT_ID: 'finances-jiewmeng',
-}
